1b2a9826fb4e7ec17a32289057837c50701c2fff
Ksdfs <khanh.nn.64cntt@ntu.edu.vn>
2025-03-25 10:06:55 +0700

    TongHopGK

---
+++ TongHopGK/src/main/java/com/LoginWebApp/controllers/WebController.java
@@ -0,0 +1,28 @@
+package com.LoginWebApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String home() {
+        return "homepage";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/list")
+    public String list() {
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String addNew() {
+        return "add";
+    }
+}
